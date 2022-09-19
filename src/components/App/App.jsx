d58819d10db74cd8b3eb9c4bfc63bb9e2c54f66d
Yana Peshko <yanapeshko917@gmail.com>
2022-09-19 12:03:08 -0700

    +UserMenu

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,9 +7,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
+      <canvas></canvas>
       React homework template
     </div>
   );
