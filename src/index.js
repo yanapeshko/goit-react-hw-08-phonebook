32787117557d4c290ac8c167a5bf472997e34b80
Yana Peshko <yanapeshko917@gmail.com>
2022-09-20 13:20:55 -0700

    +br

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-
+import { BrowserRouter } from 'react-router-dom';
 import { store } from './redux/store';
 import App from './components/App';
 import '../src/shared/styles/styles.css';
@@ -9,7 +9,10 @@ import '../src/shared/styles/styles.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter basename="goit-react-hw-08-phonebook">
+        {' '}
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
